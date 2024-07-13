8b9b3d688694a810e475cd233dbb190a2a0463bb
Isaquehg <isaqueh.goncalves@gmail.com>
2024-07-13 14:47:38 -0300

    feat: swift app created

---
+++ front-end/sportify/sportify/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  sportify
+//
+//  Created by Isaque Hollanda Goncalves on 01/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ front-end/sportify/sportify/sportifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  sportifyApp.swift
+//  sportify
+//
+//  Created by Isaque Hollanda Goncalves on 01/07/24.
+//
+
+import SwiftUI
+
+@main
+struct sportifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
