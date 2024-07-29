0f453f36a554ceba50e5b44be29beba3bbe34eb0
Isaquehg <isaqueh.goncalves@gmail.com>
2024-07-29 17:54:06 -0300

    chore: login view created

--- front-end/sportify/sportify/Views/LoginView.swift
+++ front-end/sportify/sportify/Views/LoginView.swift
@@ -9,7 +9,82 @@ import SwiftUI
 
 struct LoginView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Color.white.ignoresSafeArea()
+            VStack {
+                Image(.logo)
+                    .resizable()
+                    .frame(width: 190, height: 190)
+                    .padding(30)
+                    
+                
+                TextField("E-mail", text: .constant(""))
+                    .padding()
+                    .foregroundColor(.green)
+                    .background(Color(.systemGray5))
+                    .cornerRadius(10)
+                
+                TextField("Password", text: .constant(""))
+                    .padding()
+                    .foregroundColor(.green)
+                    .background(Color(.systemGray5))
+                    .cornerRadius(10)
+                
+                HStack {
+                    Text("Forgot your password?")
+                        .foregroundColor(.black)
+                    Text("Click here")
+                        .foregroundColor(.green)
+                }
+                
+                Text("Let's Play!")
+                    .font(.title2)
+                    .foregroundColor(.white)
+                    .padding()
+                    .frame(width: 350, height: 57.0)
+                    .background(Color.green)
+                    .cornerRadius(10)
+                
+                Divider()
+                    .padding()
+                
+                // OAuth 2.0
+                Button {
+                    print("Edit button was tapped")
+                } label: {
+                    Label("Login with Apple                         ", systemImage: "apple.logo")
+                        .padding()
+                        .foregroundStyle(.white)
+                        .background(.black)
+                        .cornerRadius(10)
+                        .font(.title2)
+                }
+                
+                Button {
+                    print("Edit button was tapped")
+                } label: {
+                    Label("Login with Facebook                 ", systemImage: "person.2.circle.fill")
+                        .padding()
+                        .foregroundStyle(.white)
+                        .background(.blue)
+                        .cornerRadius(10)
+                        .font(.title2)
+                }
+                
+                Button {
+                    print("Edit button was tapped")
+                } label: {
+                    Label("Login with Google                      ", systemImage: "circle.hexagonpath.fill")
+                        .padding()
+                        .foregroundStyle(.white)
+                        .background(.red)
+                        .cornerRadius(10)
+                        .font(.title2)
+                }
+                
+                
+            }.padding()
+        }
     }
 }
 
