8cdb3eb665c666d68e30d05dce75674826aa9274
Isaquehg <isaqueh.goncalves@gmail.com>
2024-08-14 16:58:31 -0300

    chore: login logic

--- front-end/sportify/sportify/Services/AuthService.swift
+++ front-end/sportify/sportify/Services/AuthService.swift
@@ -6,3 +6,57 @@
 //
 
 import Foundation
+
+class AuthService {
+    
+    static let shared = AuthService()
+    
+    private init() {}
+    
+    func login(email: String, password: String, completion: @escaping (Result<String, Error>) -> Void) {
+        let url = URL(string: "https://yourapi.com/login")! // todo
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        let body: [String: Any] = [
+            "email": email,
+            "password": password
+        ]
+        
+        request.httpBody = try? JSONSerialization.data(withJSONObject: body, options: .fragmentsAllowed)
+        
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            if let error = error {
+                completion(.failure(error))
+                return
+            }
+            
+            guard let data = data else {
+                completion(.failure(NSError(domain: "", code: -1, userInfo: nil)))
+                return
+            }
+            
+            do {
+                let json = try JSONSerialization.jsonObject(with: data, options: .allowFragments) as? [String: Any]
+                if let token = json?["token"] as? String {
+                    self.saveToken(token)
+                    completion(.success(token))
+                } else {
+                    completion(.failure(NSError(domain: "", code: -1, userInfo: nil)))
+                }
+            } catch {
+                completion(.failure(error))
+            }
+        }.resume()
+    }
+    
+    private func saveToken(_ token: String) {
+        UserDefaults.standard.set(token, forKey: "jwtToken")
+    }
+    
+    func getToken() -> String? {
+        return UserDefaults.standard.string(forKey: "jwtToken")
+    }
+}
--- front-end/sportify/sportify/ViewModels/LoginViewModel.swift
+++ front-end/sportify/sportify/ViewModels/LoginViewModel.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+
+class LoginViewModel: ObservableObject {
+    
+    @Published var email: String = ""
+    @Published var password: String = ""
+    @Published var isLoggedIn: Bool = false
+    @Published var errorMessage: String?
+    
+    func login() {
+        AuthService.shared.login(email: email, password: password) { 
+            [weak self] result in DispatchQueue.main.async {
+                switch result {
+                    case .success(let token):
+                        print("Logged in with token: \(token)")
+                        self?.isLoggedIn = true
+                    case .failure(let error):
+                        self?.errorMessage = error.localizedDescription
+                        self?.isLoggedIn = false
+                }
+            }
+        }
+    }
+    
+}
